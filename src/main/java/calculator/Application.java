b573b803d78eaa9d2dc0ea29cb9f56ee63882d18
dzcoffee <dzcoffee66@naver.com>
2025-10-17 16:12:57 +0900

    feat(Application) : 구분자에 따라 문자열 구분하기
    우테코 1주차 프리코스 과정에 맞는 환경을 테스트하고
    
    Application.java 파일에 구분자에 따라 문자열을 구분하고 숫자의 합을
    구하여 출력하도록 구현하였습니다.
    
    우테코 권장 자바 코드 컨벤션에 따라 추후 리팩토링이 진행될 예정입니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String st = Console.readLine();
+        String[] arr = st.split(":");
+        if(arr.length == 1) arr = st.split(",");
+
+        int answer = 0;
+        for(String number : arr){
+            int num = Integer.parseInt(number);
+            answer += num;
+        }
+
+        System.out.println("결과 : " + answer);
     }
 }
