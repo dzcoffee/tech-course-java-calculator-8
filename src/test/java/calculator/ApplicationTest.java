a1068249fd2538cc9248045787fec28d701fa30c
dzcoffee <dzcoffee66@naver.com>
2025-10-19 11:55:34 +0900

    test(Application) : 전반적인 테스트케이스를 작성해보았습니다.
    각 상황과 예외에 따른 테스트 케이스를 추가로 작성해보았습니다.
    테스트 케이스의 추가로 코드를 리팩토링하거나 서비스 로직을 수정했을 때
    빠르게 QA가 가능해집니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,9 +19,57 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+    
+    @Test
+    void 숫자_하나() {
+        assertSimpleTest(() -> {
+            run("5");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 쉼표_구분자() {
+        assertSimpleTest(() -> {
+            run("1,2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 콜론_구분자() {
+        assertSimpleTest(() -> {
+            run("1:2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 쉼표_콜론_혼용_구분자() {
+        assertSimpleTest(() -> {
+            run("1,2:3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_합계() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2;3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 연속된_구분자() {
+        assertSimpleTest(() -> {
+            run("1,2,,3"); // 1 + 2 + (0) + 3 = 6
+            assertThat(output()).contains("결과 : 6");
+        });
     }
 
     @Override
